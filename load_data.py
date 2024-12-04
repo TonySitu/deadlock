651830b6846a61a047b86b8b6f1a558ce477a1b7
Tony <137113805+TonySitu@users.noreply.github.com>
2024-12-03 20:55:28 -0500

    added insert_match_stats_info and insert_hero_stats_info

--- load_data.py
+++ load_data.py
@@ -1,20 +1,43 @@
 import sqlite3
 
-
 DATA_FILE = 'data.csv'
 DATABASE = 'deadlock.db'
 
 
 def insert_player_info(cursor, data) -> None:
     cursor.execute("""
-        INSERT INTO player (player_id) VALUES (?);
-    """, (data[0]))
+        INSERT OR IGNORE INTO player (player_id) VALUES (?);
+    """, (data[0],))
 
 
 def insert_hero_info(cursor, data) -> None:
     cursor.execute("""
-        INSERT INTO hero (hero_id, hero_name) VALUES (?, ?);
-    """, (data[1], data[2]))
+        INSERT OR IGNORE INTO hero (hero_id, hero_name) VALUES (?, ?);
+    """, (data[1], data[2],))
+
+
+def insert_match_stats_info(cursor, data) -> None:
+    cursor.execute("""
+        INSERT OR IGNORE INTO match_stats (
+            player_id,
+            hero_id,
+            match_id,
+            match_mmr,
+            match_kills,
+            match_deaths,
+            match_assists,
+            match_souls_per_minute,
+            damage,
+            win_loss
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """, (
+        data[0], data[1], data[3], data[4], data[8], data[9], data[10], data[5], data[6], data[7],))
+
+
+def insert_hero_stats(cursor, data) -> None:
+    cursor.execute("""
+            INSERT OR IGNORE INTO hero_stats (player_id, hero_id) VALUES (?, ?);
+        """, (data[0], data[1],))
 
 
 def main():
@@ -29,6 +52,8 @@ def main():
                 separated_values = [value.strip() for value in line.split(',')]
                 insert_player_info(cursor, separated_values)
                 insert_hero_info(cursor, separated_values)
+                insert_hero_stats(cursor, separated_values)
+                insert_match_stats_info(cursor, separated_values)
 
     except FileNotFoundError:
         print('File was not found')
